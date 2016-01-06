9f45ecf89685b2280a099db46e1e03b38294604a
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2016-01-07 00:34:59 +0200

    !N index page for test project for kapital traiding

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web_portal',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -90,12 +91,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
-
-# Enable Connection Pooling (if desired)
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
